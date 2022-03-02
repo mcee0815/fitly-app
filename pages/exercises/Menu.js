513fe2343f6cab5b33910e14c0cde5e61a1d95c2
mike <mikecee0815@gmail.com>
2022-03-01 22:49:44 -0500

    placeholder icons

--- pages/exercises/Menu.js
+++ pages/exercises/Menu.js
@@ -1,24 +1,38 @@
 import Link from "next/link";
 import styles from "./Menu.module.css"
+import Image from "next/image";
 
 
 
 const Menu = () => {
     return(
+        <>
+        
+    <div className={styles.menuGrid}>
+        <Link href="/exercises"><a className={styles.btn}>All</a></Link>
+        
+        <Link href="/exercises/bodyPart/back">
+            <a className={styles.btn}>
+            <Image src="/upperarms.svg" width={65} height={55} alt="logo"/>
+                    Back
+            </a>    
+        </Link>
+
+        <Link href="/exercises/bodyPart/cardio"><a className={styles.btn}><Image src="/cardio.svg" width={65} height={55} alt="logo"/>Cardio</a>
+        </Link>
+
+        <Link href="/exercises/bodyPart/chest"><a className={styles.btn}><Image src="/cardio.svg" width={65} height={55} alt="logo"/>Chest</a></Link>
+        <Link href="/exercises/bodyPart/lowerarms"><a className={styles.btn}><Image src="/cardio.svg" width={65} height={55} alt="logo"/>Lower Arms</a></Link>
+        <Link href="/exercises/bodyPart/lowerlegs"><a className={styles.btn}><Image src="/cardio.svg" width={65} height={55} alt="logo"/>lowerlegs</a></Link>
+        <Link href="/exercises/bodyPart/neck"><a className={styles.btn}>neck</a></Link>
+        <Link href="/exercises/bodyPart/shoulder"><a className={styles.btn}><Image src="/shoulders.svg" width={65} height={55} alt="logo"/>shoulder</a></Link>
+        <Link href="/exercises/bodyPart/upperarms"><a className={styles.btn}><Image src="/upperarms.svg" width={65} height={55} alt="logo"/>UpperArms</a></Link>
+        <Link href="/exercises/bodyPart/upperlegs"><a className={styles.btn}><Image src="/upperarms.svg" width={65} height={55} alt="logo"/>UpperLegs</a></Link>
+        <Link href="/exercises/bodyPart/waist"><a className={styles.btn}><Image src="/upperarms.svg" width={65} height={55} alt="logo"/> Waist</a></Link>
+    </div>
+        </>
        
-        <div className={styles.menuGrid}>
-            <Link href="/exercises"><a className={styles.btn}>All</a></Link>
-            <Link href="/exercises/bodyPart/back"><a className={styles.btn}>Back</a></Link>
-            <Link href="/exercises/bodyPart/cardio"><a className={styles.btn}>Cardio</a></Link>
-            <Link href="/exercises/bodyPart/chest"><a className={styles.btn}>Chest</a></Link>
-            <Link href="/exercises/bodyPart/lowerarms"><a className={styles.btn}>Lower Arms</a></Link>
-            <Link href="/exercises/bodyPart/lowerlegs"><a className={styles.btn}>lowerlegs</a></Link>
-            <Link href="/exercises/bodyPart/neck"><a className={styles.btn}>neck</a></Link>
-            <Link href="/exercises/bodyPart/shoulder"><a className={styles.btn}>shoulder</a></Link>
-            <Link href="/exercises/bodyPart/upperarms"><a className={styles.btn}>UpperArms</a></Link>
-            <Link href="/exercises/bodyPart/upperlegs"><a className={styles.btn}>UpperLegs</a></Link>
-            <Link href="/exercises/bodyPart/waist"><a className={styles.btn}>Waist</a></Link>
-        </div>
+        
         
     )
 }
